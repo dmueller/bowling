43985c487fabc84ed2266c0ffca2a231b72faf4a
Daniel Mueller <danielmueller@gmail.com>
2012-02-08 21:47:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bowling::Application.config.secret_token = '3016335b799f7d42211f67c3403452e1ea3321de584559bf0e45386f57240c56370159e2c5fd7f63af651d44ee80245a44df2e1713227c449d62d5662ab782b1'
