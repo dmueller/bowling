16468a1deff1a395fa26fcf0ee76fa30c180cccd
Daniel Mueller <danielmueller@gmail.com>
2012-02-25 22:08:33 -0500

    finished problems on chapter 5  * another helper method  * some more testing on the layout

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -26,4 +26,28 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "sign up")
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "about")
+
+    click_link "Contact"
+    response.should have_selector('title', :content => "contact")
+
+    click_link "Home"
+    response.should have_selector('title', :content => "home")
+
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "sign up")
+
+    click_link "About"
+    response.should have_selector('title', :content => "about")
+
+    click_link "About"
+    response.should have_selector('title', :content => "about")
+
+    
+    
+  end
+
 end
